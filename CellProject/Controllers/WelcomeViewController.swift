274f76c5a42a4d6368f0b6df14bb7801bb36e060
Anthony Howe <anthowe33@gmail.com>
2020-04-27 20:04:01 -0400

    Bypass Resgister/LoginViews

--- CellProject/Controllers/WelcomeViewController.swift
+++ CellProject/Controllers/WelcomeViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 import Firebase
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
+    
+    let db = Firestore.firestore()
+    
+    var data: [DataToStore] = [DataToStore(email: "")]
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -22,22 +26,67 @@ class WelcomeViewController: UIViewController {
         navigationController?.isNavigationBarHidden = false
         
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+        passwordTextField.isSecureTextEntry = true
+        
+    }
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
    
-
-}
-    override func viewDidAppear(_ animated: Bool){
-     super.viewDidAppear(animated)
-     if Auth.auth().currentUser != nil {
-       self.performSegue(withIdentifier: "alreadyLoggedIn", sender: nil)
-    }
-    }
     @IBAction func registerButtonPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "registerView", sender: self)
+            self.passwordTextField.isSecureTextEntry = true
+            if let email = emailTextField.text, let password = passwordTextField.text {
+                    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                        if let e = error {
+                           print(e)
+                        } else {
+                            //Navigate to the ChatViewController
+                            self.performSegue(withIdentifier: "alreadyLoggedIn", sender: self)
+                        }
+                    }
+                //MARK - Adding db functionality
+                if let emailText = Auth.auth().currentUser?.email{
+                    db.collection(K.FStore.email).addDocument(data: [K.FStore.email: emailText]) { (error) in
+                        if let e = error {
+                            print("There was an issue saving data to Firestore: \(e)")
+                        }else{
+                            print("Successfully saved data")
+                        }
+                    }
+                }
+                //!emailText.isEmpty else {return}
+                //guard let passwordText = passwordTextField.text, !passwordText.isEmpty else {return}
+                //let dataToSave: [String: Any] = ["email": emailText]
+                //db.collection(K.FStore.email)
+           
+            }
     }
     @IBAction func logInPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "logInView", sender: self)
+        if let email = emailTextField.text, let password = passwordTextField.text
+        {Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+            if let e = error{
+                print(e)
+            }else{
+                self.performSegue(withIdentifier: "alreadyLoggedIn", sender: self)
+            }
+
+            }
+
+
+        }
+    }
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        if Auth.auth().currentUser != nil {
+            self.performSegue(withIdentifier: "alreadyLoggedIn", sender: nil)
+        }
     }
 }
